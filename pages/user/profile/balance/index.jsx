f61601206a30ac8a19005f65992fcc8690513528
Henrottin Loïc <82212633+Shikibata@users.noreply.github.com>
2023-01-26 12:19:13 +0100

    Update index.jsx

--- pages/user/profile/balance/index.jsx
+++ pages/user/profile/balance/index.jsx
@@ -2,7 +2,6 @@ import PrimaryLayout from '../../../../components/layouts/primary/PrimaryLayout'
 import styled from '@emotion/styled';
 import Wire from '../../../../components/Wire/Wire';
 import { useEffect, useState } from 'react';
-import { router } from 'next/client';
 
 export default function Index() {
   const [profileId, setProfileId] = useState(null);
